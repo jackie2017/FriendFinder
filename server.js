242c2511deca979dd309546d5821912cae4afc57
Jacqueline Young <jackiey1966@gmail.com>
2017-04-29 00:05:21 -0400

    to date progress

---
+++ server.js
@@ -0,0 +1,25 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var path = require('path')
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// parse application/x-www-form-urlencoded 
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json 
+app.use(bodyParser.json())
+
+/*app.use(function (req, res){
+	res.setHeader('Content-Type', 'text/plain')
+	res.write('you posted:\n')
+	res.end(JSON.stringify(req.body, null, 2))
+});
+ */
+require('./app/routing/apiRoutes.js')(app);
+require('./app/routing/htmlRoutes.js')(app);
+
+app.listen(PORT, function(){
+	console.log("App listening on PORT: " + PORT);
+});
